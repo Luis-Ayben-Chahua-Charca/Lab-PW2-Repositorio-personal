3595868cb8c7358ea46c6c11084b57a43699c4e0
Luis-Ayben-Chahua-Charca <91394363+Luis-Ayben-Chahua-Charca@users.noreply.github.com>
2022-04-29 12:06:06 -0500

    commit 2

--- src/main/HolaMundo.java
+++ src/main/HolaMundo.java
@@ -4,5 +4,6 @@ package main;
 public class HolaMundo{
     public static void main(String args[]){
         System.out.println("Hola Mundo");
+        System.out.println("bienvenido a pw2");
     }
 }
